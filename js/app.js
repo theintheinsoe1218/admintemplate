c9b4c03a6e04bbf8e17ffb453b2f1b7c501e047e
theintheinsoe1218 <theintheinsoe1218@gmail.com>
2022-04-09 12:22:00 +0630

    add activities quck status section and footer section

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+});
+
+const getyear=document.getElementById('getyear');
+const date=new Date();
+const getfullyear=date.getFullYear();
+getyear.textContent=getfullyear;
